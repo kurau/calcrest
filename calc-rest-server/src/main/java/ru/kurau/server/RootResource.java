2b9aa72dc88419e64db0c6a05cd42791b782fa66
kurau <kurau@yandex-team.ru>
2016-09-22 15:01:10 +0300

    fix - remove consumes type

--- calc-rest-server/src/main/java/ru/kurau/server/RootResource.java
+++ calc-rest-server/src/main/java/ru/kurau/server/RootResource.java
@@ -3,14 +3,11 @@ package ru.kurau.server;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import ru.kurau.server.CalcParser;
-
-import java.net.URLDecoder;
 
 import static java.lang.String.format;
 
@@ -23,7 +20,6 @@ public class RootResource {
     @POST
     @Path("/qlate")
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.TEXT_HTML)
     public String calculateItToo(String expression) {
         return format("{\"result\": \"%s\"}", evaluate(expression));
     }
